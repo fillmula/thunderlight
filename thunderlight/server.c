65f97d597c072f8b89aaaae64bf3575e25903892
Victor Teo <victor.teo@fillmula.com>
2022-01-17 15:45:29 +0800

    messages

--- thunderlight/server.c
+++ thunderlight/server.c
@@ -45,17 +45,36 @@ PyObject *Server_listen(Server *self) {
     Py_DECREF(kwargs);
     PyObject *run_until_complete = PyObject_GetAttrString(loop, "run_until_complete");
     PyObject *server = PyObject_CallOneArg(run_until_complete, server_coro);
-
+    // port is used
     if (server == NULL) {
         PyErr_Clear();
         long port = PyLong_AsLong(self->port);
         printf("\033[1;34m[THUNDERLIGHT]\033[22;0m \033[1;31m[ERROR]\033[22;0m port %d is used.\n", port);
         Py_RETURN_NONE;
     }
+    // port is not used and server is created successfully
     PyObject *run_forever = PyObject_GetAttrString(loop, "run_forever");
+    PyObject *add_signal_handler = PyObject_GetAttrString(loop, "add_signal_handler");
+    PyObject *stop = PyObject_GetAttrString(loop, "stop");
+    PyObject *sigterm = PyLong_FromLong(SIGTERM);
+    PyObject *sigint = PyLong_FromLong(SIGINT);
+    PyObject *sigterm_args = PyTuple_New(2);
+    PyTuple_SetItem(sigterm_args, 0, sigterm);
+    PyTuple_SetItem(sigterm_args, 1, stop);
+    PyObject_Call(add_signal_handler, sigterm_args, NULL);
+    PyObject *sigint_args = PyTuple_New(2);
+    PyTuple_SetItem(sigint_args, 0, sigint);
+    PyTuple_SetItem(sigint_args, 1, stop);
+    PyObject_Call(add_signal_handler, sigint_args, NULL);
+    Py_DECREF(sigterm_args);
+    Py_DECREF(sigint_args);
+    Py_DECREF(sigterm);
+    Py_DECREF(sigint);
+    // run forever never return
     long port = PyLong_AsLong(self->port);
     printf("\033[1;34m[THUNDERLIGHT]\033[22;0m \033[1;33m[INFO]\033[22;0m server is started, listening on port %d.\n", port);
     PyObject *none = PyObject_CallNoArgs(run_forever);
+    printf("\n\033[1;34m[THUNDERLIGHT]\033[22;0m \033[1;33m[INFO]\033[22;0m server is stopped, see you.\n");
     Py_RETURN_NONE;
 }
 
